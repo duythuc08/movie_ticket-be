5c680f8b09fbaf54b8c028af2de5d0c50a999e00
Nguyen Duy Thuc <duythuc08tt@gmail.com>
2026-05-11 20:24:25 +0700

    chore: khởi tạo cấu trúc dự án và cấu hình

---
+++ src/test/java/com/example/movie_ticket_be/MovieTicketBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.movie_ticket_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieTicketBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
